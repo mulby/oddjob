6cb9dd3ad5987d0cf78c7adf221467266a8a4901
Gabe Mulley <gabe@edx.org>
2014-02-25 13:30:24 -0500

    Add provided hadoop dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
                  [org.clojure/data.csv "0.1.2"]]
   :license {:name "Apache License, Version 2.0 "
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :profiles {:dev {:resource-paths ["test-resources"]
-                   :dependencies [[org.apache.hadoop/hadoop-streaming "0.20.2"]]}}
+  :profiles {:dev 
+               {:resource-paths ["test-resources"]
+                :dependencies [[org.apache.hadoop/hadoop-streaming "0.20.2"]]}
+             :provided
+               {:dependencies [[org.apache.hadoop/hadoop-streaming "0.20.2"]]}}
   :aot :all)
