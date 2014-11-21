aecdd67a54ba71603cd2de1f85fa3dd4d7a72ae2
Gabe Mulley <gabe@edx.org>
2014-11-21 08:38:16 -0500

    Hadoop 2.3.0 support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oddjob "1.0.1"
+(defproject oddjob "1.1.0"
   :description "Hadoop utilities for MrJob that must run in the JVM"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.2"]
@@ -7,7 +7,11 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :profiles {:dev 
                {:resource-paths ["test-resources"]
-                :dependencies [[org.apache.hadoop/hadoop-streaming "0.20.2"]]}
+                :dependencies [[org.apache.hadoop/hadoop-streaming "2.3.0"]
+                               [org.apache.hadoop/hadoop-common "2.3.0"]
+                               [org.apache.hadoop/hadoop-mapreduce-client-core "2.3.0"]]}
              :provided
-               {:dependencies [[org.apache.hadoop/hadoop-streaming "0.20.2"]]}}
+               {:dependencies [[org.apache.hadoop/hadoop-streaming "2.3.0"]
+                               [org.apache.hadoop/hadoop-common "2.3.0"]
+                               [org.apache.hadoop/hadoop-mapreduce-client-core "2.3.0"]]}}
   :aot :all)
